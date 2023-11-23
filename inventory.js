ab13bd9efb7a11bf20be572b9200b2c4e034bd12
Ravindra_Reddy <reddyravindrat4512@gmail.com>
2023-11-23 11:18:46 +0530

    problem5

--- inventory.js
+++ inventory.js
@@ -63,8 +63,5 @@ let inventory = [
 
 module.exports = inventory;
 
-// ==== Problem #5 ====
-// The car lot manager needs to find out how many cars are older than the year 2000. Using the array you just obtained from the previous problem, find out how many cars were made before the year 2000 and return the array of older cars and log its length.
-
 // ==== Problem #6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Execute a function and return an array that only contains BMW and Audi cars.  Once you have the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
---
+++ problem5.js
@@ -0,0 +1,16 @@
+// ==== Problem #5 ====
+/* The car lot manager needs to find out how many cars are older than the year 2000. 
+Using the array you just obtained from the previous problem, 
+find out how many cars were made before the year 2000 and return the array of older cars and log its length. */
+
+function problem5(cars, yearsList, year) {
+  let olderCars = [];
+  yearsList.filter((years, idx) => {
+    if (years < year) {
+      olderCars.push(cars[idx]);
+    }
+  });
+  return olderCars;
+}
+
+module.exports = problem5;
